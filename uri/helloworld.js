8dd599f4d04d107cb76abb4df5d7a16c16e1040b
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-08-08 21:37:42 -0300

    iniciando hello world

---
+++ uri/helloworld.js
@@ -0,0 +1,6 @@
+//System.out.print("hello world");
+
+///confirm("alguma pergunta")
+// uri -> node v8.4 ler informacao do usuario na linha de comando
+// lab -> node v12.7 
+console.log("hello world")
