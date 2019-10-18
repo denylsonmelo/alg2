dde54fe3b915ae67cf20c4d49419c959e7cbb8fd
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-10-17 21:37:44 -0300

    multidimensional array

--- array/alunos.js
+++ array/alunos.js
@@ -3,30 +3,37 @@ var strings = require('./../biblioteca/strings');
 var colegio = require('./../biblioteca/colegio');
 
 const qtdAlunos = 2;
-var nome = new Array(qtdAlunos), 
+
+var nome = new Array(qtdAlunos),
     bimestre1 = new Array(qtdAlunos),
     bimestre2 = new Array(qtdAlunos);
+
+var alunos = [
+    nome, // ['aluno1', 'aluno2']
+    bimestre1, // [ 2 , 3 ]
+    bimestre2  // [ 4 , 6 ]
+];
 
 var entrada = terminal.escolherTerminal();
 
 terminal.imprimir('por favor informe o nome dos alunos da turma: ');
-for(var i = 0; i < nome.length ; i++){
-    terminal.imprimir(`Nome do aluno ${i+1}: `);
-    nome[i] = terminal.lerALinhaInteira(entrada);
+for (var linha = 0; linha < nome.length; linha++) {
+    terminal.imprimir(`Nome do aluno ${linha+1}: `);
+    nome[linha] = terminal.lerALinhaInteira(entrada);
 }
 
 terminal.imprimir('por favor informe a nota do bimestre 1 dos alunos da turma: ');
-for(var i = 0; i < nome.length ; i++){
-    terminal.imprimir(`Nota do "${nome[i]}": `);
-    bimestre1[i] = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
+for (var linha = 0; linha < nome.length; linha++) {
+    terminal.imprimir(`Nota do "${nome[linha]}": `);
+    bimestre1[linha] = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
 
 }
 
 terminal.imprimir('por favor informe a nota do bimestre 2 dos alunos da turma: ');
-for(var i = 0; i < nome.length ; i++){
-    terminal.imprimir(`Nota do "${nome[i]}": `);
-    bimestre2[i] = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
+for (var linha = 0; linha < nome.length; linha++) {
+    terminal.imprimir(`Nota do "${nome[linha]}": `);
+    bimestre2[linha] = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
 
 }
 
-colegio.imprimirBoletim(nome, bimestre1, bimestre2);
+colegio.imprimirBoletim(alunos);
--- biblioteca/colegio.js
+++ biblioteca/colegio.js
@@ -1,28 +1,37 @@
-exports.imprimirBoletim = function(alunos, bimestre1, bimestre2){
+exports.imprimirBoletim = function (alunos) {
     console.log('imprimindo .....');
 
     console.log('\t_________________________________________')
     console.log(`\t|  Alunos\t-----\tBim 1\t-----\tBim 2\t-----\tMedia  |`);
     console.log('\t_________________________________________')
     var media = 0;
-    for(var i=0; i < alunos.length; i++){
-        media = (bimestre1[i] + bimestre2[i])/2;
-        console.log(`\t|  ${alunos[i]}\t-----\t${bimestre1[i]}\t-----\t${bimestre2[i]}\t-----\t${media}  |`);
+
+    // aluno1, aluno2
+    // 2 , 3
+    // 4 , 6
+    
+    // aluno1 , 2, 4
+
+    for (var i = 0; i < alunos.length; i++) {
+        for (var j = 0; j < alunos[i].length; j++) {
+            media = (alunos[i][j] + alunos[i][j]) / 2;
+            console.log(`\t|  ${alunos[i][j]}\t-----\t${alunos[i+1][j]}\t-----\t${alunos[i+2][j]}\t-----\t${media}  |`);
+        }
     }
     console.log('\t_________________________________________')
 }
 
-exports.verificarSituacaoAlunoRegular = function(media){
-    if(media >= 7){
+exports.verificarSituacaoAlunoRegular = function (media) {
+    if (media >= 7) {
         return 'Aluno aprovado.';
-    } else if(media < 5) {
+    } else if (media < 5) {
         return 'Aluno reprovado.';
-    } else if (media >= 5 && media <= 6.9){
+    } else if (media >= 5 && media <= 6.9) {
         return 'Aluno em exame.';
     }
 }
 
-exports.verificarSituacaoAlunoFinal = function(media){
+exports.verificarSituacaoAlunoFinal = function (media) {
     if (media >= 5) {
         return 'Aluno aprovado.';
     } else if (media <= 4.9) {
