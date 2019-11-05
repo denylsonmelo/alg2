bb327b9e5ec69120d591a6d56da8431370aebdc0
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-11-05 20:06:00 -0300

    terminando metodo de carregamento de alunos e materias do arquivo

--- arquivo/academico.js
+++ arquivo/academico.js
@@ -1,6 +1,6 @@
-function carregarDados(arquivo){
-    const linhas = arquivo.split('\n');
-    const primeiraLinha = linhas[0].split(';');
+function carregarDados(string){
+    const linhas = strings.quebrarString(string, '\n'); 
+    const primeiraLinha = strings.quebrarString(linhas[0], ';');
 
     const qtdAlunos = strings.converterStringParaInteiro(primeiraLinha[0]);
     const qtdMaterias = strings.converterStringParaInteiro(primeiraLinha[1]);
@@ -9,11 +9,24 @@ function carregarDados(arquivo){
     const alunos = [];
 
     for(let i =1; i<= qtdAlunos; i++){
-        aluno = linhas[i].split(';');
+        aluno = strings.quebrarString(linhas[i], ';'); 
         alunos.push(aluno);
     }
 
-    console.log(alunos)
+    
+    let materia;
+    const materias = [];
+    
+    for(let i = (qtdAlunos+1); i <= (qtdMaterias + qtdAlunos); i++){
+        materia = strings.quebrarString(linhas[i], ';');
+        materias.push(materia);
+    }
+
+    return {alunos, materias}; // JSON
+}
+
+function prepararDadosSalvar(alunos, materias){
+    alunos.lenght + ";" materias.lenght
 }
 
 const terminal = require('./../biblioteca/terminal');
@@ -26,6 +39,10 @@ const localArquivo ='arquivo/bd.csv';
 //const entrada = terminal.escolherTerminal();
 //terminal.lerALinhaInteira(entrada)
 const bd = fs.readFileSync(localArquivo, 'utf-8');
+const dados = carregarDados(bd);
+const alunos = dados.alunos;
+const materias = dados.materias;
 
+const dadosPreparados = prepararDadosSalvar(alunos, materias);
+fs.writeFileSync(localArquivo, dadosPreparados,'utf-8');
 
-carregarDados(bd);
