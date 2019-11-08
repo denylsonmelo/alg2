910371334ac5cc756706ef8b5829fcee6e5435cc
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2019-11-07 21:30:25 -0300

    preparando os dados para salvar no arquivo

--- arquivo/academico.js
+++ arquivo/academico.js
@@ -1,4 +1,5 @@
 function carregarDados(string){
+    //string = Buffer.from(string, 'base64').toString()
     const linhas = strings.quebrarString(string, '\n'); 
     const primeiraLinha = strings.quebrarString(linhas[0], ';');
 
@@ -26,7 +27,18 @@ function carregarDados(string){
 }
 
 function prepararDadosSalvar(alunos, materias){
-    alunos.lenght + ";" materias.lenght
+    let dados = `${alunos.length};${materias.length}\n`;
+
+    for(let i = 0; i< alunos.length; i++){
+        dados = dados.concat(`${alunos[i]}\n`);
+    }
+
+    for(let i = 0; i < materias.length; i++){
+        dados = dados.concat(`${materias[i]}\n`);
+    }
+    dados = dados.split(',').join(';');
+    //dados = Buffer.from(dados).toString('base64')
+    return dados;
 }
 
 const terminal = require('./../biblioteca/terminal');
@@ -34,7 +46,7 @@ const strings = require('./../biblioteca/strings');
 const colegio = require('./../biblioteca/colegio');
 
 const fs = require('fs');
-const localArquivo ='arquivo/bd.csv';
+const localArquivo ='arquivo/bd';
 
 //const entrada = terminal.escolherTerminal();
 //terminal.lerALinhaInteira(entrada)
@@ -42,6 +54,10 @@ const bd = fs.readFileSync(localArquivo, 'utf-8');
 const dados = carregarDados(bd);
 const alunos = dados.alunos;
 const materias = dados.materias;
+
+//alunos.push([7, 'novo nome', 4.5, 6]);
+// processamento
+
 
 const dadosPreparados = prepararDadosSalvar(alunos, materias);
 fs.writeFileSync(localArquivo, dadosPreparados,'utf-8');
